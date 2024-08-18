1b7521de3687b275aae17e8aa8aa1b54dac2eaac
Gkkumar2 <gkk4678@gmail.com>
2024-08-18 23:08:02 +0530

    Created Document Editor Page Authentication - Clerk, Real-Time, Features - Liveblocks, Authenticate User with Real-time Features, HomePage  Collaborative Editor Room

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,15 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { Editor } from '@/components/editor/Editor'
+
+import React from 'react'
+
+const Documents = () => {
+  return (
+    <main className='flex w-full flex-col items-center'>
+     <CollaborativeRoom/>
+      
+    </main>
+  )
+}
+
+export default Documents
---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import React from "react";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Editor } from "./editor/Editor";
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">Share</p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
